c31bb7cd5afc587a93a9aee8323e4b2e5217ef11
Aryamanraj <20cs3011@rgipt.ac.in>
2026-01-27 00:31:31 +0530

    feat(types): add document types and PROSPECT_PERSON_SNAPSHOT kind

--- src/common/types/document.types.ts
+++ src/common/types/document.types.ts
@@ -5,11 +5,15 @@
 export enum DocumentKind {
   LINKEDIN_PROFILE = 'linkedin_profile',
   LINKEDIN_POSTS = 'linkedin_posts',
+  PROSPECT_SEARCH = 'prospect_search',
+  PROSPECT_SEARCH_RESULTS = 'prospect_search_results',
+  PROSPECT_PERSON_SNAPSHOT = 'prospect_person_snapshot',
   // Future: X_PROFILE, X_POSTS, GITHUB_PROFILE, etc.
 }
 
 export enum DocumentSource {
   LINKEDIN = 'LINKEDIN',
   MANUAL = 'MANUAL',
+  PROSPECT = 'PROSPECT',
   // Future: X, GITHUB, etc.
 }
