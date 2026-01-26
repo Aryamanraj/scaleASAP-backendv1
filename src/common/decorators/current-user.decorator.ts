c7357ed24de76997f055e133c01a9981acba58c3
Aryamanraj <20cs3011@rgipt.ac.in>
2026-01-27 00:33:58 +0530

    refactor(common): update decorator and remove deprecated types file

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -3,7 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export interface CurrentUserData {
   userId: number;
   email: string;
-  companyId: number;
+  clientId: number;
   role: string;
 }
 
